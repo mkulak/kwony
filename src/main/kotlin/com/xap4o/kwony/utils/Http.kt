d9600b98dd50546748c0383dffd8e7e79db8e484
mkulak <kleshney@gmail.com>
2017-04-30 00:03:04 +0200

    HttpClient.expect

--- src/main/kotlin/com/xap4o/kwony/utils/Http.kt
+++ src/main/kotlin/com/xap4o/kwony/utils/Http.kt
@@ -3,8 +3,10 @@ package com.xap4o.kwony.utils
 import com.xap4o.kwony.http.HttpClient
 import com.xap4o.kwony.http.HttpRequest
 import io.vertx.core.MultiMap
+import io.vertx.core.buffer.Buffer
 import io.vertx.core.http.HttpServerResponse
 import io.vertx.core.json.Json
+import io.vertx.ext.web.client.HttpResponse
 import java.util.Base64
 import java.util.concurrent.CompletableFuture
 
@@ -23,6 +25,12 @@ fun basicAuthHeader(name: String, password: String): Pair<String, String> =
 fun oauth2Header(token: String): Pair<String, String> =
         "Authorization" to "Bearer $token"
 
+fun HttpClient.expect(req: HttpRequest): CompletableFuture<HttpResponse<Buffer>> =
+        execute(req).map {
+            if (it.statusCode() in 200..299) it
+            else throw RuntimeException("Unexpected return code: ${it.statusCode()}")
+        }
+
 inline fun <reified T> HttpClient.json(req: HttpRequest): CompletableFuture<T> =
-        execute(req).map { it.bodyAsJson(T::class.java) }//.onError { it.printStackTrace() }
+        expect(req).map { it.bodyAsJson(T::class.java) }
 
