14dcb940d91a15245af1841322001d79118d06e1
mkulak <kleshney@gmail.com>
2017-05-02 23:54:06 +0200

    talk preparation

--- src/main/kotlin/CouroutinesTalk.kt
+++ src/main/kotlin/CouroutinesTalk.kt
@@ -5,25 +5,21 @@ import kotlin.coroutines.experimental.createCoroutine
 import kotlin.coroutines.experimental.suspendCoroutine
 
 fun main(args: Array<String>) {
-    println("start")
-    val g = gen {
+    val g = generate {
         for (i in 0..10) {
-            if (i % 3 == 0) {
-                for (j in -1..1) {
-                    yield(i + j)
-                }
-                yield(i * 10)
+            for (j in -1..1) {
+                yield(i + j)
             }
-            yield(-i)
+            yield(-i * 10)
         }
     }
-    repeat(120) {
+    repeat(10) {
         println(g.next())
     }
 }
 
 
-fun gen(block: suspend GenImpl.() -> Unit): Gen {
+fun generate(block: suspend GenImpl.() -> Unit): Gen {
     return GenImpl().apply {
         cont = block.createCoroutine(completion = this, receiver = this)
     }
@@ -35,6 +31,8 @@ interface Gen {
 }
 
 class GenImpl : Gen, Continuation<Unit> {
+    override val context: CoroutineContext = EmptyCoroutineContext
+
     lateinit var cont: Continuation<Unit>
 
     var curValue: Int = 0
@@ -44,8 +42,6 @@ class GenImpl : Gen, Continuation<Unit> {
     }
 
     override fun resumeWithException(exception: Throwable) = throw exception
-
-    override val context: CoroutineContext = EmptyCoroutineContext
 
     override fun next(): Int {
         cont.resume(Unit)
@@ -57,22 +53,6 @@ class GenImpl : Gen, Continuation<Unit> {
         suspendCoroutine<Unit> { c -> cont = c }
     }
 }
-
-//what is stack, frame lifecycle
-//how coroutines allows to "step out" of the frame saving it's state and then resume later.
-//interface Continuation<T> { //simplified for bravity
-//    fun resume(value: T)
-//    fun resumeWithException(exception: Throwable)
-//}
-//coroutines are not necessarily async!
-
-//example generagtor in python
-//simple generator in Kotlin without coroutines
-//adding coroutines
-
-//bonus: make it extend iterator
-//bonus: make lazy tree traversal?
-//bonus: talk about continuing coroutine execution with result (compiler magic!)
 
 
 
