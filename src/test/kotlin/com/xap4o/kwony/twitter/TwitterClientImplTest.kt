a23685f07c1b5f7d1faffb8f6f413de72be76675
mkulak <kleshney@gmail.com>
2017-05-07 22:58:30 +0200

    test for search

--- src/test/kotlin/com/xap4o/kwony/twitter/TwitterClientImplTest.kt
+++ src/test/kotlin/com/xap4o/kwony/twitter/TwitterClientImplTest.kt
@@ -1,5 +1,6 @@
 package com.xap4o.kwony.twitter
 
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.xap4o.kwony.config.TwitterConfig
 import com.xap4o.kwony.http.Form
 import com.xap4o.kwony.http.HttpClient
@@ -10,22 +11,29 @@ import com.xap4o.kwony.utils.Try
 import io.vertx.core.buffer.impl.BufferFactoryImpl
 import io.vertx.core.http.HttpMethod
 import io.vertx.core.json.Json
+import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
 import kotlinx.coroutines.experimental.runBlocking
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import java.net.URL
 import java.time.Duration
 
 class TwitterClientImplTest {
-    @Test
-    fun `open should return valid twitter token`() {
+    init {
+        Json.mapper.registerKotlinModule()
+    }
+
+    val config = TwitterConfig(URL("https://localhost:8080"), "key", "secret", Duration.ofSeconds(10))
+
+    @Test fun `open should return valid twitter token`() {
         runBlocking {
-            val config = TwitterConfig(URL("https://localhost:8080"), "key", "secret", Duration.ofSeconds(10))
             val mockHttpClient = TestHttpClient { req ->
                 assertThat(req.method).isEqualTo(HttpMethod.POST)
                 assertThat(req.url.toString()).isEqualTo("${config.host}/oauth2/token")
                 assertThat(req.body).isEqualTo(Form(mapOf("grant_type" to "client_credentials")))
+                assertTrue(req.headers.containsKey("Authorization"))
                 JsonObject(mapOf("access_token" to "123", "token_type" to "t")).toResponse()
             }
             val twitterClient = TwitterClientImpl(config, mockHttpClient)
@@ -34,11 +42,26 @@ class TwitterClientImplTest {
         }
     }
 
-    @Test
-    fun search() {
+    @Test fun `search should find tweets`() {
+        runBlocking {
+            val token = "123"
+            val keyword = "cheese"
+            val mockHttpClient = TestHttpClient { req ->
+                assertThat(req.method).isEqualTo(HttpMethod.GET)
+                assertThat(req.url.toString()).isEqualTo("${config.host}/1.1/search/tweets.json")
+                assertThat(req.params["q"]).isEqualTo(keyword)
+                assertThat(req.headers["Authorization"]).endsWith(token)
+                JsonObject(mapOf("statuses" to JsonArray(), "search_metadata" to JsonObject(mapOf("count" to 0, "query" to keyword)))).toResponse()
+            }
+            val twitterClient = TwitterClientImpl(config, mockHttpClient)
+
+            val result = twitterClient.search(TwitterToken(token), Keyword(keyword))
+            assertThat(result).isEqualTo(Success(SearchResponse(emptyList(), SearchMetadata(0, keyword))))
+        }
     }
 
 }
+
 
 val bufferFactory = BufferFactoryImpl()
 
